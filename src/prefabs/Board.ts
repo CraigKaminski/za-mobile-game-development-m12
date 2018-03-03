a6a9e8458bc731e868df001c478fb30a8977f379
Craig Kaminski <craig.j.kaminski@gmail.com>
2018-03-03 10:27:03 -0500

    Make Board prefab's reserve grid the same saize as grid
    Implement Board prefab logic to detect chains and remove chains

--- src/prefabs/Board.ts
+++ src/prefabs/Board.ts
@@ -1,4 +1,7 @@
-const ReserveRows = 5;
+interface IGridCoord {
+  row: number;
+  col: number;
+}
 
 export class Board {
   private blockVariations: number;
@@ -25,7 +28,7 @@ export class Board {
     this.populateGrid();
 
     this.reserveGrid = [];
-    for (let i = 0; i < ReserveRows; i++) {
+    for (let i = 0; i < rows; i++) {
       this.reserveGrid.push([]);
 
       for (let j = 0; j < cols; j++) {
@@ -51,6 +54,101 @@ export class Board {
     console.log(output);
   }
 
+  public swap(source: IGridCoord, target: IGridCoord) {
+    const temp = this.grid[target.row][target.col];
+    this.grid[target.row][target.col] = this.grid[source.row][source.col];
+    this.grid[source.row][source.col] = temp;
+  }
+
+  private checkAdjacent(source: IGridCoord, target: IGridCoord) {
+    const diffRow = Math.abs(source.row - target.row);
+    const diffCol = Math.abs(source.col - target.col);
+
+    return (diffRow === 1 && diffCol === 0) || (diffRow === 0 && diffCol === 1);
+  }
+
+  private clearChains() {
+    const chainedBlocks: IGridCoord[] = this.findAllChains();
+
+    chainedBlocks.forEach((block) => {
+      this.grid[block.row][block.col] = 0;
+    });
+  }
+
+  private dropBlock(sourceRow: number, targetRow: number, col: number) {
+    this.grid[targetRow][col] = this.grid[sourceRow][col];
+    this.grid[sourceRow][col] = 0;
+  }
+
+  private dropReserveBlock(sourceRow: number, targetRow: number, col: number) {
+    this.grid[targetRow][col] = this.reserveGrid[sourceRow][col];
+    this.reserveGrid[sourceRow][col] = 0;
+  }
+
+  private findAllChains() {
+    const chained: IGridCoord[] = [];
+
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        const coord = {row: i, col: j};
+        if (this.isChained(coord)) {
+          chained.push(coord);
+        }
+      }
+    }
+
+    return chained;
+  }
+
+  private isChained(block: IGridCoord) {
+    let isChained = false;
+    const row = block.row;
+    const col = block.col;
+    const variation = this.grid[row][col];
+
+    // left
+    if (variation === this.grid[row][col - 1] && variation === this.grid[row][col - 2]) {
+      isChained = true;
+    }
+
+    // right
+    if (variation === this.grid[row][col + 1] && variation === this.grid[row][col + 2]) {
+      isChained = true;
+    }
+
+    // up
+    if (
+      this.grid[row - 2]
+      && (variation === this.grid[row - 1][col] && variation === this.grid[row - 2][col])
+    ) {
+      isChained = true;
+    }
+
+    // down
+    if (
+      this.grid[row + 2]
+      && (variation === this.grid[row + 1][col] && variation === this.grid[row + 2][col])
+    ) {
+      isChained = true;
+    }
+
+    // center - horizontal
+    if (variation === this.grid[row][col - 1] && variation === this.grid[row][col + 1]) {
+      isChained = true;
+    }
+
+    // center -vertical
+    if (
+      this.grid[row - 1]
+      && this.grid[row + 1]
+      && (variation === this.grid[row - 1][col] && variation === this.grid[row + 1][col])
+    ) {
+      isChained = true;
+    }
+
+    return isChained;
+  }
+
   private populateGrid() {
     for (let i = 0; i < this.rows; i++) {
       for (let j = 0; j < this.cols; j++) {
@@ -61,11 +159,42 @@ export class Board {
   }
 
   private populateReserveGrid() {
-    for (let i = 0; i < ReserveRows; i++) {
+    for (let i = 0; i < this.rows; i++) {
       for (let j = 0; j < this.cols; j++) {
         const variation = Math.floor(Math.random() * this.blockVariations) + 1;
         this.reserveGrid[i][j] = variation;
       }
     }
+  }
+
+  private updateGrid() {
+    let foundBlock = false;
+
+    for (let i = this.rows - 1; i >= 0; i--) {
+      for (let j = 0; j < this.cols; j++) {
+        if (this.grid[i][j] === 0) {
+          foundBlock = false;
+
+          for (let k = i - 1; k >= 0; k--) {
+            if (this.grid[k][j] > 0) {
+              this.dropBlock(k, i, j);
+              foundBlock = true;
+              break;
+            }
+          }
+
+          if (!foundBlock) {
+            for (let k = this.rows - 1; k >= 0; k--) {
+              if (this.reserveGrid[k][j] > 0) {
+                this.dropReserveBlock(k, i, j);
+                break;
+              }
+            }
+          }
+        }
+      }
+    }
+
+    this.populateReserveGrid();
   }
 }
