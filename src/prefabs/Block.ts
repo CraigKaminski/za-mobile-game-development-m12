e21f3630c9120d38c9f8166555767070dc56a24e
Craig Kaminski <craig.j.kaminski@gmail.com>
2018-03-04 08:00:13 -0500

    Add resetData method to Block prefab to allow blocks to be reused

--- src/prefabs/Block.ts
+++ src/prefabs/Block.ts
@@ -1,15 +1,31 @@
-interface IBlockData {
+export interface IBlockData {
   asset: string;
+  col: number;
+  row: number;
 }
 
-export class Block extends Phaser.Sprite{
+export class Block extends Phaser.Sprite {
+  private col: number;
+  private row: number;
   private state: Phaser.State;
 
   constructor(state: Phaser.State, x: number, y: number, data: IBlockData) {
     super(state.game, x, y, data.asset);
 
     this.state = state;
+    this.row = data.row;
+    this.col = data.col;
 
     this.anchor.setTo(0.5);
+  }
+
+  public resetData(x: number, y: number, data: IBlockData) {
+    super.reset(x, y);
+
+    this.loadTexture(data.asset);
+    this.row = data.row;
+    this.col = data.col;
+
+    return this;
   }
 }
